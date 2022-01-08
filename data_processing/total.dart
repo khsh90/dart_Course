98e297ffab80b956381ffe1ac9f637cf565c34ed
Khaled Hadidi <khsh400@gmail.com>
2022-01-09 00:05:45 +0200

    finishing data processing

--- data_processing/total.dart
+++ data_processing/total.dart
@@ -1,44 +1,67 @@
 import 'dart:io';
 
-void main(List<String> arguments) {
-  if (arguments.isEmpty) {
+void main(List<String> argumesnts) {
+  if (argumesnts.isEmpty) {
     print('usage :dart total.dart <input.csv>');
-    exit(1);
+    exit(1); //exit and show exit 1 for (exit with issue wile 0 extis without 0)
   }
-  final input = arguments.first;
+
+  final input = argumesnts.first;
   final lines = File(input).readAsLinesSync();
-  final totalDurationByTag = <String, double>{};
-
   lines.removeAt(0);
-
+  var totalActivitesByTag = <String, double>{};
+  var totalDuration = 0.0;
   for (var line in lines) {
-    final values = line.split(',');
-    final durationStr = values[3].replaceAll('"', '');
-    final duration = double.parse(durationStr);
-    final tag = values[5].replaceAll('"', '');
-    var previousTagvalue = totalDurationByTag[tag];
+    final vlaues = line.split(',');
 
-    if (previousTagvalue == null) {
-      previousTagvalue = duration;
+    final durationStr = vlaues[3].replaceAll('"', '');
+    var duration = double.parse(durationStr);
+    totalDuration += duration;
+    var tag = vlaues[5].replaceAll('"', '');
+    final previousTag = totalActivitesByTag[tag];
+    if (previousTag == null) {
+      totalActivitesByTag[tag] = duration;
     } else {
-      totalDurationByTag[tag] = previousTagvalue + duration;
+      totalActivitesByTag[tag] = previousTag + duration;
     }
+    for (var item in totalActivitesByTag.entries) {
+      final formatedValues = item.value.toStringAsFixed(1);
+      final tag1 = item.key == '' ? 'Undefined' : item.key;
 
-    for (var entry in totalDurationByTag.entries) {
-      final properFormatedValue = entry.value.toStringAsFixed(1);
-      final keyTag = entry.key;
-
-      print('$tag :$properFormatedValue');
+      print('Tag $tag1:Duration $formatedValues hour');
     }
-
+    print('Total all tags:${totalDuration.toStringAsFixed(1)} Hour');
   }
 }
 
-//lines=File[input]
-//durationByMap=empty map {}
-//line.removeAt(0) in order to delete the ""
-//for (va line in lines){
-//values=line.split(',')
-//duration=value[3]
-//taf=value[5]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------
+
+//input = File (list).syncAsline
+//file lines.split(',')
+//totalActivitisBytag=map;
+//for var line in lines;
+//final values=line
+//var durationStr =value[3].replaceAll('"','');
+//duration=double.parse(durationStr);
+
+//tag=value[5].replaceAll('"','');
+
+//if totalActivitisBytag[tag]==null let equal duration
+//for(vlaue in vlaues.entitiy){
+//print key and vlaues.
 //}
