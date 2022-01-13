ca218e0c15fbdac4ac41250d097cae7f65e56d50
Khaled Hadidi <khsh400@gmail.com>
2022-01-14 00:09:42 +0200

    finish advanved functions

---
+++ functionAdvanced/combine.dart
@@ -0,0 +1,31 @@
+void main() {
+  var emails = [
+    'khaled@gmail.com',
+    'ahmad@yahoo.com',
+    'ahmad@gmaail.com',
+    'wqq@llol.com'
+  ];
+
+  var knownDomains = ['gmail.com', 'yahoo.com'];
+
+  var filter = filterDomains(emails, knownDomains);
+
+  print(filter);
+  // ---------------------------------------------
+  // another way using for in loop
+
+  var unknownDomain = [];
+  for (var email in emails) {
+    var lastEmailDomain = email.split('@').last;
+    if (!knownDomains.contains(lastEmailDomain))
+      unknownDomain.add(lastEmailDomain);
+  }
+
+  print(unknownDomain);
+}
+
+Iterable<String> filterDomains(
+        List<String> emails, List<String> knownDomains) =>
+    emails
+        .map((e) => e.split('@').last)
+        .where((element) => !knownDomains.contains(element));
