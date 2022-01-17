e53e32a1b9497b16af3283b9789adb1987b70aad
Khaled Hadidi <khsh400@gmail.com>
2022-01-17 23:17:58 +0200

    basic class

---
+++ classbasic/l1.dart
@@ -0,0 +1,32 @@
+class BankAccount {
+  double balance = 0; //this called property
+
+  double deposite(double amount) {
+    balance += amount;
+    return balance;
+  }
+
+  bool withdrow(double amount) {
+    if (balance >= amount) {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
+}
+
+void main() {
+  final bankAccout1 = BankAccount();
+
+  bankAccout1.deposite(20);
+  bankAccout1.deposite(100);
+  //print(bankAccout1.balance);
+
+  final sucess1 = bankAccout1.withdrow(20);
+  final deposite1 = 'Sucess:$sucess1, the balance is:${bankAccout1.balance}';
+  final sucess2 = bankAccout1.withdrow(500);
+
+  final deposite2 = 'sucess:$sucess2,the balance is: ${bankAccout1.balance}';
+  print(deposite1);
+  print(deposite2);
+}
